79c873d26e4b5a581d65988ed02205c4b4f9fa7a
DominikDolphin <dominik.thibaudeau@gmail.com>
2023-09-28 22:22:40 -0400

    fixed crashing issue for post when sending unsupported content-type

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,4 +1,4 @@
-const { createSuccessResponse } = require('../../response.js');
+const { createSuccessResponse, createErrorResponse } = require('../../response.js');
 const { Fragment } = require('../../model/fragment.js');
 const logger = require('./../../logger.js');
 /**
@@ -6,26 +6,42 @@ const logger = require('./../../logger.js');
  */
 module.exports = async (req, res) => {
   // Get buffer size.
-  let buffer = Buffer.from(req.body);
-  let bufferSize = buffer.length;
 
-  // Create the fragment
-  const fragment = new Fragment({
-    ownerId: req.user,
-    type: req.headers['content-type'],
-    size: bufferSize,
-  });
+  try {
+    // If the content-type is not supported, this handles it and prevents the app from crashing.
+    if (Buffer.isBuffer(req.body) === false || req.body == {}) {
+      logger.debug('POST did not send proper buffer. Content-type is not be supported.');
+      return res.send(createErrorResponse(406, 'Content-type not supported.'));
+    }
 
-  await fragment.save();
-  await fragment.setData(buffer);
+    let buffer = Buffer.from(req.body);
 
-  //Respond with the fragment.
-  res.setHeader('Content-Location', `${process.env.API_URL || req.headers.host}/v1/fragments/${fragment.id}`);
-  res.status(201).json(
-    createSuccessResponse({
-      fragment: fragment,
-    })
-  );
+    let bufferSize = buffer.length;
 
-  logger.debug({ fragment }, `Created new fragment`);
+    // Create the fragment
+    const fragment = new Fragment({
+      ownerId: req.user,
+      type: req.headers['content-type'],
+      size: bufferSize,
+    });
+
+    await fragment.save();
+    await fragment.setData(buffer);
+
+    //Respond with the fragment.
+    res.setHeader(
+      'Content-Location',
+      `${process.env.API_URL || req.headers.host}/v1/fragments/${fragment.id}`
+    );
+
+    logger.debug({ fragment }, `Created new fragment`);
+
+    return res.status(201).json(
+      createSuccessResponse({
+        fragment: fragment,
+      })
+    );
+  } catch (err) {
+    throw new Error('Error creating new fragment');
+  }
 };
